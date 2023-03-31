d9dbffc7161fc5c0b2a9748db0363391b1d54c52
Will Thomas <30wthomas@gmail.com>
2023-03-30 23:01:16 -0500

    Updating dependencies and fixing ignore

--- MarkUpInterp.js
+++ MarkUpInterp.js
@@ -1,69 +1,75 @@
-;
-var MarkupNodeType;
-(function (MarkupNodeType) {
-    MarkupNodeType["H1"] = "h1";
-    MarkupNodeType["H2"] = "h2";
-    MarkupNodeType["H3"] = "h3";
-    MarkupNodeType["P"] = "p";
-    MarkupNodeType["BR"] = "br";
-})(MarkupNodeType || (MarkupNodeType = {}));
-;
-var TextBox = document.querySelector("#textBox");
-var RootDiv = document.querySelector("#root");
-var displayMode = false;
-var FileToString = function (filePath) {
-    var fileString = "";
-    return fileString;
-};
-var NodeParser = function (inputString) {
-    var lineList = inputString.split("\n");
-    var nodeList = new Array();
-    lineList.forEach(function (line) {
-        if (line.startsWith("###")) {
-            nodeList.push({ data: line.substr(3), type: MarkupNodeType.H3 });
-        }
-        else if (line.startsWith("##")) {
-            nodeList.push({ data: line.substr(2), type: MarkupNodeType.H2 });
-        }
-        else if (line.startsWith("#")) {
-            nodeList.push({ data: line.substr(1), type: MarkupNodeType.H1 });
-        }
-        else if (line === "") {
-            nodeList.push({ data: "", type: MarkupNodeType.BR });
-        }
-        else {
-            nodeList.push({ data: line, type: MarkupNodeType.P });
-        }
-    });
-    return nodeList;
-};
-var Interp = function (inputProg) {
-    var RootElem = document.createElement("div");
-    RootElem.id = "interpRoot";
-    inputProg.forEach(function (value) {
-        var newElem = document.createElement(value.type);
-        newElem.innerText = value.data;
-        RootElem.appendChild(newElem);
-    });
-    return RootElem;
-};
-var SwapModes = function () {
-    displayMode = !displayMode;
-    TextBox.hidden = displayMode;
-    RootDiv.hidden = !displayMode;
-    document.title = "MarkUp - " + (displayMode ? "Viewer" : "Interpreter");
-};
-window.addEventListener("keydown", function (e) {
-    if (e.ctrlKey && e.key == "Enter") {
-        if (displayMode) {
-            RootDiv.firstChild.remove();
-            SwapModes();
-            return;
-        }
-        var textBoxInput = TextBox.value;
-        var nodes = NodeParser(textBoxInput);
-        var htmlInterp = Interp(nodes);
-        RootDiv.appendChild(htmlInterp);
-        SwapModes();
-    }
-});
+"use strict";
+// interface AbstractMarkUpNode {
+//     data: string;
+//     type: MarkupNodeType;
+// };
+// enum MarkupNodeType {
+//     H1 = "h1",
+//     H2 = "h2",
+//     H3 = "h3",
+//     P = "p",
+//     BR = "br",
+// };
+// const TextBox = document.querySelector("#textBox") as HTMLInputElement;
+// const RootDiv = document.querySelector("#root") as HTMLDivElement;
+// let displayMode = false;
+// const FileToString = (filePath: string): string => {
+//     // Read file to string
+//     let fileString = "" // File read into one big string
+//     return fileString;
+// }
+// const NodeParser = (inputString: string): AbstractMarkUpNode[] => {
+//     // We need to parse line by line (probably at least for now)
+//     let lineList = inputString.split("\n");
+//     let nodeList = new Array<AbstractMarkUpNode>();
+//     lineList.forEach((line: string) => {
+//         if (line.startsWith("###")) {
+//             // H3
+//             nodeList.push({ data: line.substr(3), type: MarkupNodeType.H3 })
+//         } else if (line.startsWith("##")) {
+//             // H2
+//             nodeList.push({ data: line.substr(2), type: MarkupNodeType.H2 })
+//         } else if (line.startsWith("#")) {
+//             // H1
+//             nodeList.push({ data: line.substr(1), type: MarkupNodeType.H1 })
+//         } else if (line === "") {
+//             // Break
+//             nodeList.push({ data: "", type: MarkupNodeType.BR })
+//         } else {
+//             nodeList.push({ data: line, type: MarkupNodeType.P });
+//         }
+//     })
+//     return nodeList;
+// }
+// const Interp = (inputProg: AbstractMarkUpNode[]): HTMLElement => {
+//     const RootElem = document.createElement("div");
+//     RootElem.id = "interpRoot";
+//     inputProg.forEach((value: AbstractMarkUpNode) => {
+//         let newElem = document.createElement(value.type);
+//         newElem.innerText = value.data;
+//         RootElem.appendChild(newElem);
+//     })
+//     return RootElem;
+// }
+// const SwapModes = () => {
+//     displayMode = !displayMode;
+//     TextBox.hidden = displayMode;
+//     RootDiv.hidden = !displayMode;
+//     document.title = "MarkUp - " + (displayMode ? "Viewer" : "Interpreter");
+// }
+// window.addEventListener("keydown", (e) => {
+//     if (e.ctrlKey && e.key == "Enter") {
+//         if (displayMode) {
+//             // Restore and return
+//             RootDiv.firstChild.remove();
+//             SwapModes();
+//             return;
+//         }
+//        let textBoxInput = TextBox.value;
+//        let nodes = NodeParser(textBoxInput);
+//        let htmlInterp = Interp(nodes);
+//        RootDiv.appendChild(htmlInterp);
+//        SwapModes();
+//     }
+// })
+//# sourceMappingURL=MarkUpInterp.js.map
--- MarkUpInterp.ts
+++ MarkUpInterp.ts
@@ -1,80 +1,80 @@
-interface AbstractMarkUpNode {
-    data: string;
-    type: MarkupNodeType;
-};
+// interface AbstractMarkUpNode {
+//     data: string;
+//     type: MarkupNodeType;
+// };
 
-enum MarkupNodeType {
-    H1 = "h1",
-    H2 = "h2",
-    H3 = "h3",
-    P = "p",
-    BR = "br",
-};
+// enum MarkupNodeType {
+//     H1 = "h1",
+//     H2 = "h2",
+//     H3 = "h3",
+//     P = "p",
+//     BR = "br",
+// };
 
-const TextBox = document.querySelector("#textBox") as HTMLInputElement;
-const RootDiv = document.querySelector("#root") as HTMLDivElement;
-let displayMode = false;
+// const TextBox = document.querySelector("#textBox") as HTMLInputElement;
+// const RootDiv = document.querySelector("#root") as HTMLDivElement;
+// let displayMode = false;
 
-const FileToString = (filePath: string): string => {
-    // Read file to string
-    let fileString = "" // File read into one big string
-    return fileString;
-}
+// const FileToString = (filePath: string): string => {
+//     // Read file to string
+//     let fileString = "" // File read into one big string
+//     return fileString;
+// }
 
-const NodeParser = (inputString: string): AbstractMarkUpNode[] => {
-    // We need to parse line by line (probably at least for now)
-    let lineList = inputString.split("\n");
-    let nodeList = new Array<AbstractMarkUpNode>();
-    lineList.forEach((line: string) => {
-        if (line.startsWith("###")) {
-            // H3
-            nodeList.push({ data: line.substr(3), type: MarkupNodeType.H3 })
-        } else if (line.startsWith("##")) {
-            // H2
-            nodeList.push({ data: line.substr(2), type: MarkupNodeType.H2 })
-        } else if (line.startsWith("#")) {
-            // H1
-            nodeList.push({ data: line.substr(1), type: MarkupNodeType.H1 })
-        } else if (line === "") {
-            // Break
-            nodeList.push({ data: "", type: MarkupNodeType.BR })
-        } else {
-            nodeList.push({ data: line, type: MarkupNodeType.P });
-        }
-    })
-    return nodeList;
-}
+// const NodeParser = (inputString: string): AbstractMarkUpNode[] => {
+//     // We need to parse line by line (probably at least for now)
+//     let lineList = inputString.split("\n");
+//     let nodeList = new Array<AbstractMarkUpNode>();
+//     lineList.forEach((line: string) => {
+//         if (line.startsWith("###")) {
+//             // H3
+//             nodeList.push({ data: line.substr(3), type: MarkupNodeType.H3 })
+//         } else if (line.startsWith("##")) {
+//             // H2
+//             nodeList.push({ data: line.substr(2), type: MarkupNodeType.H2 })
+//         } else if (line.startsWith("#")) {
+//             // H1
+//             nodeList.push({ data: line.substr(1), type: MarkupNodeType.H1 })
+//         } else if (line === "") {
+//             // Break
+//             nodeList.push({ data: "", type: MarkupNodeType.BR })
+//         } else {
+//             nodeList.push({ data: line, type: MarkupNodeType.P });
+//         }
+//     })
+//     return nodeList;
+// }
 
-const Interp = (inputProg: AbstractMarkUpNode[]): HTMLElement => {
-    const RootElem = document.createElement("div");
-    RootElem.id = "interpRoot";
-    inputProg.forEach((value: AbstractMarkUpNode) => {
-        let newElem = document.createElement(value.type);
-        newElem.innerText = value.data;
-        RootElem.appendChild(newElem);
-    })
-    return RootElem;
-}
+// const Interp = (inputProg: AbstractMarkUpNode[]): HTMLElement => {
+//     const RootElem = document.createElement("div");
+//     RootElem.id = "interpRoot";
+//     inputProg.forEach((value: AbstractMarkUpNode) => {
+//         let newElem = document.createElement(value.type);
+//         newElem.innerText = value.data;
+//         RootElem.appendChild(newElem);
+//     })
+//     return RootElem;
+// }
 
-const SwapModes = () => {
-    displayMode = !displayMode;
-    TextBox.hidden = displayMode;
-    RootDiv.hidden = !displayMode;
-    document.title = "MarkUp - " + (displayMode ? "Viewer" : "Interpreter");
-}
+// const SwapModes = () => {
+//     displayMode = !displayMode;
+//     TextBox.hidden = displayMode;
+//     RootDiv.hidden = !displayMode;
+//     document.title = "MarkUp - " + (displayMode ? "Viewer" : "Interpreter");
+// }
 
-window.addEventListener("keydown", (e) => {
-    if (e.ctrlKey && e.key == "Enter") {
-        if (displayMode) {
-            // Restore and return
-            RootDiv.firstChild.remove();
-            SwapModes();
-            return;
-        }
-       let textBoxInput = TextBox.value;
-       let nodes = NodeParser(textBoxInput);
-       let htmlInterp = Interp(nodes);
-       RootDiv.appendChild(htmlInterp);
-       SwapModes();
-    }
-})
+// window.addEventListener("keydown", (e) => {
+//     if (e.ctrlKey && e.key == "Enter") {
+//         if (displayMode) {
+//             // Restore and return
+//             RootDiv.firstChild.remove();
+//             SwapModes();
+//             return;
+//         }
+//        let textBoxInput = TextBox.value;
+//        let nodes = NodeParser(textBoxInput);
+//        let htmlInterp = Interp(nodes);
+//        RootDiv.appendChild(htmlInterp);
+//        SwapModes();
+//     }
+// })
