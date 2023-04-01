36e0de7d8cf06d39a43da87c3587a6d262f2b356
Will Thomas <30wthomas@gmail.com>
2023-04-01 18:06:01 -0500

    Adding katex and output

--- MarkdownTest.ts
+++ MarkdownTest.ts
@@ -73,6 +73,11 @@ const token_desc_list: TokenDescription[] = [
     precedence: 0,
   },
   {
+    name: "LATEX_SNIP",
+    description: /\$[^\$]+\$/,
+    precedence: 3,
+  },
+  {
     name: "BR",
     description: /\n/,
     precedence: 10,
@@ -80,7 +85,7 @@ const token_desc_list: TokenDescription[] = [
 ];
 
 const test_str =
-  "# Testing\n### This is a little header\nand we can **have baby** text under it\nmore *text* can be adding, how it will be parsed\nI am not quite sure.\n\nLet us see _how_ this works, __this would be bold I think__.\n Now we see that # hashes midway should be preserved.\n> Can we do blockquotes?\n>> How about nested ones\n1. This is **some stuff**\n2. More stuff\n3. Again another list item\n- Now lets try for an unordered list\n- Can we do it?\n\t- Indented list?!\n\t- Im not sure.\nNow, lets try code inside here `hello my code stuff`\n---\nA horizontal rule might be nice\nHere is a link [Duck Duck Go](https://duckduckgo.com).\n";
+  "# Testing\n### This is a little header\nand we can **have baby** text under it\nmore *text* can be adding, how it will be parsed\nI am not quite sure.\n\nLet us see _how_ this works, __this would be bold I think__.\n Now we see that # hashes midway should be preserved.\n> Can we do blockquotes?\n>> How about nested ones\n1. This is **some stuff**\n2. More stuff\n3. Again another list item\n- Now lets try for an unordered list\n- Can we do it?\n\t- Indented list?!\n\t- Im not sure.\nNow, lets try code inside here `hello my code stuff`\n---\nA horizontal rule might be nice\nHere is a link [Duck Duck Go](https://duckduckgo.com).\n$1 + 1 = 2$\n";
 const output_tokens = Tokenize(test_str, token_desc_list);
 
 const gram: Grammar<string> = [
